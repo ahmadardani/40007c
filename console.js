97290b52ff1b1da8ddd4ecd47bce0f9042339272
ahmadardani <ahmadardani2011@gmail.com>
2025-02-21 10:09:43 +0800

    .

---
+++ console.js
@@ -0,0 +1,15 @@
+{
+    function addTwoNumbers(numA, numB) {
+        return numA+numB;
+    }
+
+    const a = 5; const b = 10;
+    console.log(addTwoNumbers(a, b));
+}
+{
+    function addTwoNumber(numA, numB) {
+        return numA+numB;
+    }
+    const a=5; const b=10;
+    console.log(addTwoNumbers(a,b));
+}
